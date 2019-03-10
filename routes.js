e6da559080d8118532d2de78f22c377ed56f8121
zedonora <timeundtime@gmail.com>
2019-03-11 07:18:45 +0900

    init2

---
+++ routes.js
@@ -0,0 +1,52 @@
+// Global
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+// Users
+
+const USERS = "/users";
+const USER_DETAIL = "/:id"; // users/1
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+
+// Videos
+
+const VIDEOS = "/videos";
+const UPLOAD = "/upload";
+const VIDEO_DETAIL = "/:id"; // videos/1 => :var 은 동적인 값임을 express가 인식
+const EDIT_VIDEO = "/:id/edit"; // videos/1/edit
+const DELETE_VIDEO = "/:id/delete"; // vidoes/1/delete
+
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: id => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
+    }
+  },
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: id => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO
+};
+
+export default routes;
